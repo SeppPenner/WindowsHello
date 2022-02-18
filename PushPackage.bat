6d91a25fe4711ce44c1638f9a25d8765cce2d3e3
SeppPenner <HansM2013@gmx.de>
2022-02-18 19:14:02 +0100

    Updated build script.

--- PushPackage.bat
+++
@@ -1,3 +0,0 @@
-dotnet nuget push "src\WindowsHello\bin\Release\HaemmerElectronics.SeppPenner.WindowsHello.*.nupkg" -s "github" --skip-duplicate
-dotnet nuget push "src\WindowsHello\bin\Release\HaemmerElectronics.SeppPenner.WindowsHello.*.nupkg" -s "nuget.org" --skip-duplicate -k "%NUGET_API_KEY%"
-PAUSE
